f317392df28f1d292e272af1fd01ec3ab811c521
kujiy <kujiy.fs@gmail.com>
2021-12-24 10:13:18 +0900

    travis

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 
 docker build . -t kujiy/ocsp-tester:latest
-VERSION=$(docker run -it  kujiy/ocsp-tester:latest ocsp-tester version | awk '{print $2}')
+VERSION=$(docker run -it  kujiy/ocsp-tester:latest openssl version | awk '{print $2}')
 echo $VERSION
 docker build . -t kujiy/ocsp-tester:$VERSION
 
